1fa904cd283f8e71da2d272165a8d5b56319aeed
tranducthanh98 <47779805+tranducthanh98@users.noreply.github.com>
2024-12-27 22:46:41 +0700

    Add files via upload

---
+++ demo01/src/main/java/test.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/thanhtran/Downloads/chromedriver-mac-arm64 2/chromedriver");
+
+        WebDriver  driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
