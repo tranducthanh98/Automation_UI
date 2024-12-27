cc400eac62035407ed6b287c995afc3355be7b55
tranducthanh98 <47779805+tranducthanh98@users.noreply.github.com>
2024-12-27 23:33:26 +0700

    Add files via upload

---
+++ Automation_UI/src/test/java/test_selenium/a.java
@@ -0,0 +1,16 @@
+package test_selenium;
+
+import org.testng.annotations.BeforeTest;
+
+public class a extends AutomationTestNg{
+    @BeforeTest
+    @Override
+    public void OpenBrowser() throws InterruptedException {
+        super.OpenBrowser();
+    }
+
+    @Override
+    public void User() {
+        super.User();
+    }
+}
